b3c77e6ad150cca4d0d49be6cb06741267ff328e
cmmerritt <christiane.merritt@gmail.com>
2021-05-11 23:42:06 -0700

    three tests pass

--- src/reduce-methods.js
+++ src/reduce-methods.js
@@ -4,9 +4,30 @@ export const toLastNames = people => people.map(function(element){
   return `${element.firstName} ${element.lastName}`;
 });
 
-export function addValues(arr) {
+// trying several variations of reduce, not getting it 
+
+/* export function addValues(arr) {
   arr.reduce((a, b) => a + b, 0);
-}
+} */
+
+/* export function addValues(arr) {
+  arr.reduce(function(a, b) {
+    return a + b;
+  });
+} */
+
+/* export function addValues(arr) {
+  console.log(arr);
+  arr.reduce( (a, b) => a + b, 0 )
+}; */
+
+export const addValues = arr => arr.reduce((a, b) => a + b, 0);
+
+export const addPurchases = arr => arr.reduce((a, b) => a + b.purchasePrice, 0);
+
+
+
+
 
 
 
--- src/reduce-methods.test.js
+++ src/reduce-methods.test.js
@@ -1,10 +1,36 @@
-import { toLastNames, addValues } from './reduce-methods.js';
+import { toLastNames, addValues, addPurchases } from './reduce-methods.js';
 
 test('toLastNames', () => {
   expect(toLastNames([{ firstName:'Jane', lastName:'Doe' }, { firstName:'James', lastName:'Bond' }])).toStrictEqual(['Jane Doe', 'James Bond']);
 });
 
 test('addValues', () => {
-  console.log(addValues(57, 92, 18));
-  expect(addValues([57, 92, 18]).toStrictEqual(167));
+  console.log(addValues[57, 92, 18]);
+  expect(addValues([57, 92, 18])).toStrictEqual(167);
+});
+
+const purchases = [
+  {
+    item: 'switch',
+    purchasePrice: 399
+  },
+
+  {
+    item: 'a brain',
+    purchasePrice: 25699
+  },
+
+  {
+    item: 'a heart',
+    purchasePrice: 89563
+  },
+
+  {
+    item: 'the noive',
+    purchasePrice: 2890
+  }
+];
+
+test('addPurchases', () => {
+  expect(addPurchases(purchases)).toStrictEqual(118551);
 });
